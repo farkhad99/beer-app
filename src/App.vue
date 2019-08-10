327cdea8ee0a3c86cb2338f2754b0c7ce4f6f596
maxaquentin99 <maxaquentin@gmail.com>
2019-08-10 22:46:54 +0500

    pug format switched

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
-<template>
-  <div id="app">
-    <div id="nav">
+<template lang='pug'>
+  #app
+    #nav
       <router-link to="/">Home</router-link> |
       <router-link to="/favourites">Favourites</router-link>
-    </div>
-    <router-view/>
-  </div>
+    router-view
 </template>
 
 <style>
