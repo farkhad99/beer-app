8d44beb6dc9a45726930c1d4eeb3916ab011f9df
maxaquentin99 <maxaquentin@gmail.com>
2019-08-11 03:33:49 +0500

    Adding files

---
+++ dist/precache-manifest.0a6ecadb7342421b83db539a573617ee.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "3679dc025c1f6130951c",
+    "url": "/css/chunk-fa733d22.3b234936.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "12cc0dfa790b768f72ae",
+    "url": "/js/chunk-vendors.c4451ebf.js"
+  },
+  {
+    "revision": "3679dc025c1f6130951c",
+    "url": "/js/chunk-fa733d22.99954de5.js"
+  },
+  {
+    "revision": "3ea8576f3cd27d3b4ec6",
+    "url": "/js/chunk-69f18f46.aff4af95.js"
+  },
+  {
+    "revision": "6a6cb5cff30571ecf3bd",
+    "url": "/js/chunk-0dd51ee0.af0c937e.js"
+  },
+  {
+    "revision": "c683aaefa2f50b77b4c6",
+    "url": "/js/app.1adec38c.js"
+  },
+  {
+    "revision": "4289ece840ab26a2d48ba99b203053e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf5a1eed12ebe52bfac9a0accd47fbb5",
+    "url": "/img/black_star.cf5a1eed.png"
+  },
+  {
+    "revision": "3ea8576f3cd27d3b4ec6",
+    "url": "/css/chunk-69f18f46.c4f62c75.css"
+  },
+  {
+    "revision": "6a6cb5cff30571ecf3bd",
+    "url": "/css/chunk-0dd51ee0.be612a4b.css"
+  },
+  {
+    "revision": "c683aaefa2f50b77b4c6",
+    "url": "/css/app.a2f195dc.css"
+  }
+];
