6ef9e6725efa081ed9ce53d7063416351fd62a77
maxaquentin99 <maxaquentin@gmail.com>
2019-08-10 20:27:42 +0500

    mutations settings

--- src/store.js
+++ src/store.js
@@ -1,16 +1,51 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    beers: [],
+    favourites: []
+  },
+  getters: {
+    beers: (state) => state.beers,
+    favourites: (state) => state.favourites
   },
   mutations: {
-
+    SET_BEERS: (state, payload) => {
+      state.beers = payload
+    },  
+    SET_FAVS: (state, payload) => {
+      state.favourites = payload
+    }
   },
   actions: {
-
+    getBeers: async (ctx) => {
+      try{ 
+        let response  = await axios.get('https://api.punkapi.com/v2/beers/random')
+        ctx.commit('SET_BEERS', response.data)
+      } catch (err) {throw err}
+    },
+    searchBeers: async (ctx, beer_name) => {
+      try {
+        let response  = await axios.get('https://api.punkapi.com/v2/beers?beer_name='+beer_name)
+        ctx.commit('SET_BEERS', response.data)
+      } catch (err) {throw err}
+    },
+    getFavourites: (ctx) => {
+      try{
+        let favs      = JSON.parse(localStorage.getItem('favourites'))
+        let query_ids = favs.join('|')
+        let res       = await axios.get('https://api.punkapi.com/v2/beers/random?ids='+query_ids)
+        ctx.commit('SET_FAVS', res.data)
+      } catch(err) {throw err}
+    },
+    addToFavourites: (ctx, item) => {
+      let favs = JSON.parse(localStorage.getItem('favourites'))
+      favs.push(item)
+      localStorage.setItem('favourites',JSON.stringify(favs))      
+    }
   }
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
-<template>
+<template >
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{beers}}
+    <ItemCard msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import ItemCard from '@/components/ItemCard.vue'
+import {mapGetters} from 'vuex'
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    ItemCard
+  },
+  computed: {
+    ...mapGetters(['beers'])
+  },
+  mounted(){
+    this.$store.dispatch('getRandomBeers')
   }
 }
 </script>
